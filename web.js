e219229a140096ff1d6e9252df8fe24a381a14f1
karan rathod <92873841+karangunaji@users.noreply.github.com>
2023-01-01 13:00:10 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    loop: true,
+    grabCorsure:true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  
